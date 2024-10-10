88bddbb1d263a973a65455400f8b4a58f8cb7d7f
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-10-10 14:38:15 +0200

    Fix: output difference

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -8,26 +8,26 @@ a specified number of times.
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zoom in an array by repeating each element a specified number of times.
 
     Args:
-        lst (Tuple[int, ...]): A tuple of integers to be zoomed in.
+        lst (Tuple): A tuple of elements to be zoomed in.
         factor (int, optional): The number of times each element is repeated.
                                 Defaults to 2.
 
     Returns:
-        Tuple[int, ...]: A new tuple with each element of `lst` repeated factor
+        List: A new list with each element of `lst` repeated factor
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
-array = (12, 72, 91)  # Use tuple instead of list
+array = (12, 72, 91)  # Use tuple as input
 
 zoom_2x = zoom_array(array)
 
