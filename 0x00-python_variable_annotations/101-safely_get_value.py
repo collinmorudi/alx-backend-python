e11cf4be1aff12744cf64e40aad37678e61b2a69
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-10-10 15:15:26 +0200

    Modified the type hints

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+"""101-safely_get_value"""
+
+
 from typing import Mapping, Any, Union, TypeVar
+
 
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any, default:
                      Union[T, None] = None) -> Union[Any, T]:
+    """TODO: add details"""
     if key in dct:
         return dct[key]
     else:
